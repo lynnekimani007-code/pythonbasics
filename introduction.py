366e8394c12a26f25cdcecb5f254392e077bda30
lynnekimanioo7-code <lynnekimani007@gmail.com>
2026-02-03 13:24:47 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+print("Welcome to python programming")
+print("lynne")
+print(200)
+print(40*3)
+#print is a method used to display an output/value
+
---
+++ variables.py
@@ -0,0 +1,15 @@
+# variable is a container used to store a value
+school = "emobilis"
+firstname = "john"
+lastname ="smith"
+num1 = 45
+num2 = 100
+language ="python"
+print(school)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(school,"is a coding school")
+print(language," is a web programming language")
+print(num1)
+print("the sum of", num1,"and",num2 ,"is",num1+num2)
