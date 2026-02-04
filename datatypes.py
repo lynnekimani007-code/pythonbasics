15b9312772824cba3d4c330a4f3531f0d3a089a1
lynnekimani007-code <lynnekimani007@gmail.com>
2026-02-04 13:34:16 +0300

    new commit

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+age = 29 #integer
+length = 45.6 #float
+greeting = "hello there"#string
+hasFeathers = True #boolean
+#Data structures-multiple values stored in one variable
+#one variable.
+
+courses = ["MIT","Data science","Cybersecurity"]#array-similar datatypes
+fruits = ["apple","banana","orange","mango","pineapple"]# list-ordered and changeable -different datatypes
+cars =   ("Mercedes","Ford","Nissan","Jeep")#Tuple-ordered and unchangeable
+countries = {"Malaysia","Kenya","India"}#set-unordered and unchangeable
+student = {
+     "firstname" : "lynne",
+     "course"    : "MIT" ,
+     "age"       :  10,
+     "nationality": "Kenya",
+}#dictionary-key value pair
+
+
+print(age)
+print("The length is",length)
+print(fruits)
+print(cars)
+print(countries)
+print(student)
+#typecasting-converting one datatype to another
+print(float(age))
+print(int(length))
---
+++ strings.py
@@ -0,0 +1,8 @@
+greeting = "Hello"
+course = "SOFTWARE ENGINEERING"
+print(len(greeting))
+print(greeting[0])
+print(course.lower())
+print(greeting.upper())
+#string concatenation-joining strings
+print(greeting + course)
