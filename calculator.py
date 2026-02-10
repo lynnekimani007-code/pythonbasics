69e7165a6d944c5bee5f056246079db8b7fb8c4a
lynnekimani007-code <lynnekimani007@gmail.com>
2026-02-10 11:41:46 +0300

    new commit

--- calculator.py
+++ calculator.py
@@ -1,3 +1,24 @@
 #write a simple calc prog
 #when the prog runs it prompts the user to enter first number , operator(+,-,*,/, and second number
 #based on the operator entered it should provide the correct output
+
+num1 = float(input("Enter the first number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Division by zero"
+else:
+    result = "Invalid operator used"
+
+print("Result:", result)
+
+
